443864fa580f521ab8650d17c78d6f4badef691e
dostontv <dostontv969@gmail.com>
2024-08-10 16:01:22 +0500

    Updated

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Listing(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
