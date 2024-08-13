5816d21e7d75d7bed67d382e199fec9948da15d9
behruzgithub <aristokrat@Aristokrats-MacBook-Air.local>
2024-08-13 17:32:44 +0500

    refresh

--- root/urls.py
+++ root/urls.py
@@ -8,7 +8,7 @@ from root.settings import MEDIA_URL, MEDIA_ROOT
 urlpatterns = [
     path('api/v1/', include('apps.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     path('admin/', admin.site.urls),
