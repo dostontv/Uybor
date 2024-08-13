074da3fc907bab8f3c3df5f2631347ca4d932edb
uchiha122112 <ime37me@gmail.com>
2024-08-13 17:49:55 +0500

    nimadurla yozdm

--- apps/management/commands/listing_populate.py
+++ apps/management/commands/listing_populate.py
@@ -1,11 +1,9 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
-
-from apps.models import User
-
+from apps.models import Listing, User, Category  # User va Category modellaringizni import qiling
 
 class Command(BaseCommand):
-    help = "Closes the specified poll for voting"
+    help = "Populates the database with listings"
 
     def add_arguments(self, parser):
         parser.add_argument("user", type=int)
@@ -17,24 +15,40 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Populating database..."))
 
         for i in range(options['user']):
-            users.append(User(
-                first_name=f.name(),
-                phone_number=f"998{f.msisdn()[:9]}",
-                balance=f.random_number(digits=3) * 1000,
-                organization=f.company(),
-                type=f.random_choices(elements=User.UserType.choices, length=1)[0][0]
+            random_user = User.objects.order_by('?').first()
+            random_category = Category.objects.order_by('?').first()
+            listings.append(Listing(
+                owner=random_user,
+                category=random_category,
+                description=f.text(max_nb_chars=255),
+                place=f.address(),
+                price=f.random_number(digits=3) * 1000,
+                price_currency=f.random_element(elements=[choice[0] for choice in Listing.Currency.choices]),
+                price_type=f.random_element(elements=[choice[0] for choice in Listing.PriceType.choices]),
+                price_period_unit=f.company_suffix(),
+                room=max(1, f.random_number(digits=1) * 12),
+                is_price_auction=f.boolean(),
+                square=max(1, f.random_number(digits=1, fix_len=True)),
+                floor=max(1, f.random_number(digits=1, fix_len=True)),
+                floor_total=max(1, f.random_number(digits=1, fix_len=True)),
+                is_new_building=f.boolean(),
+                repair=f.random_element(elements=[choice[0] for choice in Listing.RepairType.choices]),
+                foundation=f.random_element(elements=[choice[0] for choice in Listing.FoundationType.choices]),
+                residential_complex_id=None,
+                moderation_status=f.random_element(elements=[choice[0] for choice in Listing.MSType.choices]),
+                urgently_expired_at=f.date_time_ad(),
+                verified_expired_at=f.date_time_ad(),
+                premium_expired_at=f.date_time_ad(),
+                vip_expired_at=f.date_time_ad(),
+                views=f.random_number(digits=2) * 1000,
+                favorites=f.random_number(digits=2) * 100,
+                is_vip=f.boolean(),
+                is_premium=f.boolean(),
+                is_urgently=f.boolean()
             ))
 
-        User.objects.bulk_create(users)
-        # for poll_id in options["poll_ids"]:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-        #
-        #     poll.opened = False
-        #     poll.save()
+        Listing.objects.bulk_create(listings)
 
         self.stdout.write(
-            self.style.SUCCESS(f"Successfully populated {options['user']} users")
+            self.style.SUCCESS(f"Successfully populated {options['user']} listings")
         )
