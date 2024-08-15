dad5a14b1f5ebdbde3df91b53e58270adbb5bba1
dostontv <dostontv969@gmail.com>
2024-08-15 14:29:47 +0500

    Added postgres and Fixed Project and Added .env

--- root/urls.py
+++ root/urls.py
@@ -4,8 +4,14 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 from root.settings import MEDIA_URL, MEDIA_ROOT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include('apps.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
