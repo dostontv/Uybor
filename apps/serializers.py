25f01d9873dccab5af109ee5f9b5e1203488b0fb
Sardor23324 <deo17ssssssssss@gmail.com>
2024-08-20 18:09:37 +0500

    teacher todo

--- apps/serializers.py
+++ apps/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField
+from rest_framework.fields import SerializerMethodField, HiddenField, CurrentUserDefault
 
 from apps.models import User, Listing, Category
 
@@ -18,11 +18,13 @@ class ListingDetailModelSerializer(serializers.ModelSerializer):
 
 
 class ListingListModelSerializer(serializers.ModelSerializer):
+    owner = HiddenField(default=CurrentUserDefault())
     image_count = SerializerMethodField(read_only=True)
 
     class Meta:
         model = Listing
-        fields = ['id', 'owner', 'description', 'image_count', 'price', 'room', 'square', 'floor', 'floor_total', 'created_at', 'update_at','views', 'is_active', 'moderation_status','operation_type']
+        fields = ['id', 'owner', 'description', 'image_count', 'price', 'room', 'square', 'floor', 'floor_total',
+                  'created_at', 'update_at', 'views', 'is_active', 'moderation_status', 'operation_type']
 
     def get_image_count(self, obj: Listing) -> int:
         return obj.images.count()
